d5c41e0f4f64a08eae7dcdcccc801fa7f7c52137
BugHunter-X <askzenai@gmail.com>
2025-01-01 17:10:05 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,30 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error because the variable 'data' is not defined
+  console.log(data);
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+```
